7822d0a29922ff8fa504d2bd6464138e07874bab
olaus-bo-hvl <olbo04199@hvl.no>
2025-11-11 17:08:12 +0100

    Oppgave 4

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -1,6 +1,8 @@
 package no.hvl.dat100.oppgave4;
 
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 import no.hvl.dat100.common.TODO;
@@ -10,6 +12,22 @@ public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+        boolean skrevet = false;
+        String fullFilsti = mappe + "/" + filnavn;
+
+		try {
+            PrintWriter skriver = new PrintWriter(new FileWriter(fullFilsti));
+
+            skriver.print(samling.toString());
+
+            skriver.close();
+            skrevet = true;
+
+        } catch (IOException e) {
+            System.out.println("Feil ved skriving til fil: " + e.getMessage());
+            skrevet = false;
+        }
+
+        return skrevet;
 	}
 }
